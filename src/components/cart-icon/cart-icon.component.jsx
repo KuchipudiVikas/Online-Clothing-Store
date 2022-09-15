24822587ed0044ae3c19ceab9ec82b4ccbc8f0dd
vikas <kuchipudivikas@gmail.com>
2022-09-15 14:47:00 +0530

    add item card and item count

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -3,12 +3,12 @@ import { useContext } from 'react';
 import './cart-icon.styles.scss'
 import { CartContext } from '../../contexts/cart.context';
 const CartIcon = () => {
-    const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+    const { isCartOpen, setIsCartOpen, cartItemCount } = useContext(CartContext);
     const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen)
     return (
         <div className="cart-icon-container" onClick={toggleIsCartOpen}>
             <ShoppingIcon className='shopping-icon' />
-            <span className='item-count'>10</span>
+            <span className='item-count'>{cartItemCount}</span>
         </div>
     )
 }
