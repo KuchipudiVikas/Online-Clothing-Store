a01793588ff23637f63864701c7f7edb6b927f1c
vikas <kuchipudivikas@gmail.com>
2022-09-16 06:34:03 +0530

    fix directory page --routing to categories

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,7 +1,10 @@
+import { useNavigate } from 'react-router-dom';
 import './directory-item.styles.scss';
 
 const DirectoryItem = ({ category }) => {
     const { imageUrl, title } = category;
+    const navigate = useNavigate()
+
     return (
         <div className='directory-item-container'>
             <div
@@ -10,7 +13,9 @@ const DirectoryItem = ({ category }) => {
                     backgroundImage: `url(${imageUrl})`,
                 }}
             />
-            <div className='body'>
+            <div className='body' onClick={() => {
+                navigate(`/shop/${title}`)
+            }}>
                 <h2>{title}</h2>
                 <p>Shop Now</p>
             </div>
